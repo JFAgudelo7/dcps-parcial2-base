04d3532693ebc2676548bc6c26572aa982d75052
JF <jfagudeloa@unal.edu.co>
2018-05-10 19:37:41 -0500

    jj

--- app/routes/authors/index.js
+++ app/routes/authors/index.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model({id}){
+        const authors = this.modelFor('author');
+        const author = authors.findBy('id', id);
+
+        if(!author){
+            this.transitionTo('author');
+        }
+        else{
+            return author;
+        }
+
+    }
 });
